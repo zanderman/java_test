671dd6275f728a841c591ad2d21229513324af1a
Alexander DeRieux <acd1797@vt.edu>
2013-09-14 17:05:51 -0400

    Changed 'HelloWorld.java' file.

--- MyApp/src/HelloWorld.java
+++ MyApp/src/HelloWorld.java
@@ -1,7 +1,14 @@
 
+// Note: this project is contained in the GitHub folder...
+// 				cd ~/Documents/GitHub/Development/java_test
 public class HelloWorld {
 	public static void main(String[] args){
-		System.out.println("XDA!");
+		int x = 0;
+		
+		while(x != 20){
+			System.out.println("X = " + x);
+			x++;	
+		}
 	}
 
 }
